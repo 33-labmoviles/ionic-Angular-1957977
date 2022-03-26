7a24f01dca28ddbad137401b83339edb3aead7fe
Roy Axel <guelcoj@live.com.mx>
2022-03-26 14:43:46 -0600

    corrigiendo

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor() {}
+
+    personas = [
+      {
+        "nombre": "Diosito",
+        "apellidos": "????",
+        "matricula": "11111111",
+        "image": "assets/images/pic.png"
+      },
+      {
+        "nombre": "Yo",
+        "apellidos": "Guel",
+        "matricula": "1957977",
+        "image": "assets/images/pic.png"
+      },
+      {
+        "nombre": "La profe",
+        "apellidos": "the best (paseme profe)",
+        "matricula": "17G35434",
+        "image": "assets/images/pic.png"
+      },
+      {
+        "nombre": "Diosito",
+        "apellidos": "????",
+        "matricula": "11111111",
+        "image": "assets/images/pic.png"
+      },
+      {
+        "nombre": "Yo",
+        "apellidos": "Guel",
+        "matricula": "1957977",
+        "image": "assets/images/pic.png"
+      },
+      {
+        "nombre": "La profe",
+        "apellidos": "the best (paseme profe)",
+        "matricula": "17G35434",
+        "image": "assets/images/pic.png"
+      },
+      {
+        "nombre": "Diosito",
+        "apellidos": "????",
+        "matricula": "11111111",
+        "image": "assets/images/pic.png"
+      },
+      {
+        "nombre": "Yo",
+        "apellidos": "Guel",
+        "matricula": "1957977",
+        "image": "assets/images/pic.png"
+      },
+      {
+        "nombre": "La profe",
+        "apellidos": "the best (paseme profe)",
+        "matricula": "17G35434",
+        "image": "assets/images/pic.png"
+      }
+    ];
+
+
+
+
+}
